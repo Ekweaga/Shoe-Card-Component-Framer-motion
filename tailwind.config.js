9183a4dad8c137b282b183ac5b3fd53c7bb7e0ae
Ekweaga <hikay133@gmail.com>
2023-01-19 15:26:38 +0100

    Install needed dependencies/library(Framer-motion)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [ "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
